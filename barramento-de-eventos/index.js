9b4fd0bdd41b0f55e00bacb35afdac292da148be
Mathvivas <mathvivas@hotmail.com>
2021-06-11 17:17:35 -0300

    Construção do Barramento de Eventos

---
+++ barramento-de-eventos/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+app.use(express.json())
+
+const eventos = []
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body
+    eventos.push(evento)
+    // Envia o evento para o microsserviço de pedidos (3000)
+    axios.post('http://localhost:3000/eventos')
+    // Envia o evento para o microsserviço de status (4000)
+    axios.post('http://localhost:4000/eventos')
+    // Envia o evento para o microsserviço de entregas (5000)
+    axios.post('http://localhost:5000/eventos')
+    // Envia o evento para o microsserviço de pagamentos (6000)
+    axios.post('http://localhost:6000/eventos')
+    // Envia o evento para o microsserviço de consultas (7000)
+    axios.post('http://localhost:7000/eventos')
+
+    res.status(200).send({ msg: 'OK' })
+})
+
+app.get('/eventos', (req, res) => {
+    req.send(eventos)
+})
+
+app.listen(10_000, () => console.log('Microsserviço do Barramento de Eventos inicado na porta 10000.'))
